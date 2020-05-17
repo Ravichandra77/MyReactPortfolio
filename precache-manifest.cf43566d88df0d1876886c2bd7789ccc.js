4473d6dad9c57c608bac88f6e52df95a38650158
Ravi Chandra <chandraravi280@gmail.com>
2020-05-17 23:06:28 +0530

    Updates

---
+++ precache-manifest.cf43566d88df0d1876886c2bd7789ccc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741157508c3be686f535a9c162306321",
+    "url": "/MyReactPortfolio/index.html"
+  },
+  {
+    "revision": "61a70c1c2fd93647e7a0",
+    "url": "/MyReactPortfolio/static/css/2.123d055b.chunk.css"
+  },
+  {
+    "revision": "fc202d12069612f4eefe",
+    "url": "/MyReactPortfolio/static/css/main.bbb5d615.chunk.css"
+  },
+  {
+    "revision": "61a70c1c2fd93647e7a0",
+    "url": "/MyReactPortfolio/static/js/2.6ad04c65.chunk.js"
+  },
+  {
+    "revision": "a6f954d13841761469d7b49406b45778",
+    "url": "/MyReactPortfolio/static/js/2.6ad04c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc202d12069612f4eefe",
+    "url": "/MyReactPortfolio/static/js/main.02222a8e.chunk.js"
+  },
+  {
+    "revision": "5ae8ef3fb5b0662ad792",
+    "url": "/MyReactPortfolio/static/js/runtime-main.d8cfe0a6.js"
+  },
+  {
+    "revision": "554bdc8cfe36a69ae881ca02c8227675",
+    "url": "/MyReactPortfolio/static/media/1.554bdc8c.gif"
+  },
+  {
+    "revision": "a8a5d490c11bec1a96a2777506ff5426",
+    "url": "/MyReactPortfolio/static/media/2.a8a5d490.gif"
+  },
+  {
+    "revision": "554bdc8cfe36a69ae881ca02c8227675",
+    "url": "/MyReactPortfolio/static/media/3.554bdc8c.gif"
+  },
+  {
+    "revision": "d684d4e200a2a38efc628f2365cc6231",
+    "url": "/MyReactPortfolio/static/media/4.d684d4e2.gif"
+  },
+  {
+    "revision": "509f70271562958df64f87d33a2e3000",
+    "url": "/MyReactPortfolio/static/media/My_pic.509f7027.jpg"
+  },
+  {
+    "revision": "4c732369e54ee14c3d884b15b441ad0f",
+    "url": "/MyReactPortfolio/static/media/angular.4c732369.svg"
+  },
+  {
+    "revision": "da53dc3418ca99ce8fec3846274d9cb8",
+    "url": "/MyReactPortfolio/static/media/bs.da53dc34.png"
+  },
+  {
+    "revision": "35c72f4aee86badd475c6c144c12982a",
+    "url": "/MyReactPortfolio/static/media/css.35c72f4a.png"
+  },
+  {
+    "revision": "5a84d5d7850aeed12df857d09493b6f9",
+    "url": "/MyReactPortfolio/static/media/js.5a84d5d7.png"
+  },
+  {
+    "revision": "d19fb978f8f2f209ffee95603e03f8ba",
+    "url": "/MyReactPortfolio/static/media/sass.d19fb978.png"
+  }
+]);
